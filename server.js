35472aa80c68fb8931f191e64b485db0032c8b4c
Meghan Simmons <simmons.meghan@gmail.com>
2023-05-09 17:55:46 -0400

    added server.js and connected to the servr

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3001;
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
+
+
+
