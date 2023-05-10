d6920dfd4e62eee08bb6351ac4c53563afa83e48
Meghan Simmons <simmons.meghan@gmail.com>
2023-05-10 19:02:37 -0400

    added HTML routes in server.js

--- server.js
+++ server.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const path = require('path');
+const api = require('./routes/index.js');
 
 const app = express();
 const PORT = 3001;
 
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+// app.use('/api', api);
 app.use(express.static('public'));
 
-
-app.listen(PORT, () =>
-  console.log(`Example app listening at http://localhost:${PORT}`)
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// GET Route for notes page
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
+);
 
+// GET route for wildcard - direct users to homepage
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+);
 
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
