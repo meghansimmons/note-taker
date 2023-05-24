1c1ef36d4143d5b640951a412a6fd8ee4bbc6aa2
Meghan Simmons <simmons.meghan@gmail.com>
2023-05-23 20:14:41 -0400

    added process.env.port to deploy to heroku"

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
